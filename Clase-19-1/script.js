8daf2f879e3e8702a88ff849342e3480337fb6ac
Lautaro Montaña <joselautarom@gmail.com>
2023-08-10 22:10:33 -0300

    Clase 19 - 1 JS

---
+++ Clase-19-1/script.js
@@ -0,0 +1,100 @@
+// Funcion prederterminada y básica para ver errores y resultados en consola del navegador
+// console.log("Soy un resultado")
+
+// Funcion prederminada y básica para ver resultados en el navegador en forma de ventana saliente
+// alert("Soy una alerta")
+
+// Tipos de datos en JavaScript 👇
+
+// 1) Los string ¿Que son?: Son tipos de datos de cadena de texto
+// Por ejemplo: "Lorenzo"
+
+// 2) Los numéricos o también llamados Number ¿Que son?: Son tipos de datos de números
+// Por ejemplo: 1 que no es lo mismo que escribir asi: "1"
+
+// Variables ¿Que son? Técnicamente una variable es un espacio reservado de almacenamiento en memoria, es un tipo de dato en donde podemos guardar casi cualquier tipo de información que querramos utilizar en algun momento en nuestro proyecto de software
+
+// ¿Que tipos de variables en JS existen?
+
+// Los tipos de variables son: var, let y const son parte de las palabras RESERVADAS del lenguaje lo que significa que solo tiene este propósito
+
+// Palabra reservada var: Es una "antigua" forma de escribir o declarar variables, pero su función tiene alcance global y al largo plazo puede ocasionar problemas
+
+// Palabras reservadas let: Introducida en la version de JS ECMAScript 6 (ES6), tiene un alcance de bloque, lo que significa que solo es visible o también su scope tiene alcance LOCAL
+
+// Palabras reservadas const: Introducida en ES6, se utiliza opara declarar variables cuyo valor no cambiará después de la asignación.
+
+// Operadores
+// ¿Que son los operadores? Es la manera en la que vamos a poder interactuar y poder hacer algoritmos con JS
+
+// _____________________________________________________________
+
+// ¿Que tipos de operadores existen?
+
+// Operador de asignación:
+
+// let nombre = "Agustín";
+
+// console.log(nombre)
+// ___________________________________________________
+
+// Operadores de cálculos aritméticos
+
+// Principales operadores aritméticos: + - * / (suma, resta, multiplicación y división)
+
+// let primerNumero = 1;
+
+// let segundoNumero = 2;
+
+// let suma = primerNumero + segundoNumero;
+
+// console.log(suma)
+
+// let tercerNumero = 4;
+
+// let cuartoNumero = 2;
+
+// let dividir = tercerNumero / cuartoNumero;
+
+// console.log(dividir)
+
+// ________________________________________________
+
+// Concatenación de textos
+
+// let primerNombre = "Pablo";
+
+// let apellido = "Bezard"
+
+// let concat = primerNombre + " " + apellido;
+
+// console.log( "Te damos la bienvenida a JS " + concat )
+
+// __________________________________________________
+
+// Pedimos datos mediante prompt (string)
+
+// let pedirNombre = prompt("Ingrese su nombre")
+
+// console.log("Hola " + pedirNombre)
+
+// Pedimos datos mediante prompt (Number o numéricos)
+
+// let edad = Number(prompt("Ingrese su edad"))
+
+// let edad = parseInt(prompt("Ingrese su edad"))
+
+// let edad = parseFloat(prompt("Ingrese su edad"))
+
+// console.log("Usted tiene " + edad + " años")
+
+
+
+
+
+
+
+
+
+
+
