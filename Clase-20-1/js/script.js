c2d05748fae3c6556ccdbe24a310ed22c98259c6
Lautaro Montaña <joselautarom@gmail.com>
2023-08-14 19:40:31 -0300

    Clase 20 - Operadores de comparacion/condición)

---
+++ Clase-20-1/js/script.js
@@ -0,0 +1,30 @@
+// Operadores de COMPARACIÓN
+
+// Estos determinan si la comparación de dos operandos, de ahora en más datos, se cumple o no...
+
+// Si se cumple, nos devuelve algo llamado: Operador Buleano Verdadero
+
+// En ingles, Boolean True
+
+// Caso contrario, nos devuelve algo llamado: Operador Buleano Verdadero
+
+// En ingles, Boolean False
+
+// ¿Cuales son los operadores de comparación más utilizados?
+
+// 1) == igual / Devuelve "true" si los datos son iguales
+
+// 2) != no es igual también conocido como "distinto" / Devuelve true si los datos no son iguales o distintos
+
+// 3) === estrictamente igual / devuelve true si los datos son iguales en tipo y valor
+
+// 4) !== desigualdad estricta / Devuelve true si los datos son del mismo tipo no son iguales o son de diferente tipo
+
+// 5) > mayor que / Devuelve true si el dato IZQUIERDO es MAYOR que el dato derecho
+
+// 6) >= mayor o igual que / Devuelve true si el dato izquierdo es mayor o igual al derecho
+
+// 7) < menor que / Devuelve true si el dato izquierdo es menor que el dato derecho
+
+// 8) <= menor que / Devuelve true si el dato izquiero es menor o igual al derecho
+
