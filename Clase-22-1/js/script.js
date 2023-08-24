47483f33ee6ec895ca0b59dfb2fcb2876544ed1a
Lautaro Montaña <joselautarom@gmail.com>
2023-08-24 20:07:23 -0300

    Clase 22 arrays y sus métodos

---
+++ Clase-22-1/js/script.js
@@ -0,0 +1,48 @@
+// Array: es una estructura de datos, que permite almacenar multiples valores en una sola variable
+// Cada valor dentro de esta variable se llama elemento y se puede accerder a el mediante un índice
+// Los indices en JS comienzan en 0, es decir el primer elemento tiene un indice de 0 y sigue el seguiente que es 1, etc.
+
+// Un variable normal se declara asi:
+
+// let variableNombre = "Chanchito";
+
+// let variableApellido = "Feliz"
+
+// Una variable tipo array
+
+// let array = ["Chanchito", "Feliz"]
+// console.log(array)
+
+// Para que se muestre el primer elemento hacemos esto:
+// console.log(array[0])
+// console.log(array[1])
+
+// Variable tipo array vacio
+
+// let arrayVacio = []
+
+// console.log(arrayVacio)
+
+// Primer método de array
+
+// const array = ["Gonzalo", "Magalí", "Damián"]
+
+// Método push()
+// Agregamos elementos al final del array
+// array.push("Lorenzo")
+
+// Método pop()
+// Eliminamos el útimo elemento del array
+// array.pop()
+
+// Método length
+// Obtenemos la longitud de un array
+// console.log(array.length)
+
+// Modificamos los elementos asignando nuevos valores a travéz de su indice
+
+// array[2] = "Andrea"
+// array[0] = 3
+
+// console.log(array)
+
