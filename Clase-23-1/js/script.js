c270b55616c4e47a66346a3050980e569116162f
Lautaro Montaña <joselautarom@gmail.com>
2023-08-31 19:53:23 -0300

    Explicación estructura de control de flujos SWITCH

---
+++ Clase-23-1/js/script.js
@@ -0,0 +1,14 @@
+// El switch es una estructura de control de flujo en programación que se utiliza para tomar decisiones basadas en el valor de una expresion o variable. En otras palabras el switch nos permite hacer que el software se ejecute en diferentes bloque de código segun el valor de una variable
+
+// Cual es el uso principal del switch? Es simplicar el código cuando se tienen multiples condiciones que deben evaluarse. En lugar de usar una serie de declaraciones, 'if...else..if...else?, podemos usar el switch para organizar y simplicar el bloque
+
+// Características claves y ventajas del switch:
+
+// 1. Evaluación única: La expresión o variable se evalua una sola vez en un switch, que es muchos que multiples if...else..if...else
+
+// 2. Casos multiples: Puede agrupar multples casos que deben tener la misma lógica o acción en un solo bloque de código
+
+// 3. Limpieza del código: Hace que el código sea LEGIBLE y facil de mantener cuando tenés muchas condiciones que deben mantenerse
+
+// 4. Uso del default: Se puede proporcionar un caso del "default" que va a ejecutar si ninguno de los bloques coincide con el valor de la expresion, lo que proporciona una forma de manejar algo inesperado
+
