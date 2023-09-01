a361df983b4e69fc62712f8107b78d4c2bb87a5c
Lautaro Montaña <joselautarom@gmail.com>
2023-08-31 22:00:32 -0300

    Terminamos con bucles for

---
+++ Clase-23-1/js/for.js
@@ -0,0 +1,41 @@
+// bucles for: en javascript son estructuras de control que te permiten ejecutar un bloque de código varias veces, generalmente para recorrer elementos de una lista, como por ejemplo un array
+
+// Un bucle "for" tiene tres partes: inicialización, condición y empresión de incremento
+
+// 1. Inicialización: Se ejecuta una vez al principio y define una variable de control
+
+// 2. Condición: Se verifica en cada iteración (vuelta). Si es "true", el bucle continúa; si el bucle es "false", termina.
+
+// 3. Expresión de incremento: Se ejecuta después de cada iteración, generalmente para actualizar la variable de control
+
+// Sintaxis básica de un bucle for
+
+// for(inicialización; condición; expresión de incremento){
+            //  Código a ejecutar en cada iteracion
+// }
+
+// console.log("1")
+// console.log("2")
+// console.log("3")
+// console.log("4")
+// console.log("5")
+
+// Vamos a imprimir del 1 al 5
+// for( let i = 1; i <= 5; i++){
+//     console.log(i)
+// }
+
+// let suma = 0;
+// for(let i =1; i<= 10; i++){
+//     suma += i;
+// }
+// console.log("La suma es: ", suma)
+
+// const array = ["manzana", "durazno", "limon"]
+
+// for( let i = 0;  i < array.length; i++){
+//     console.log(array[i])
+// }
+
+
+
